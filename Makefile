0e18bee835c8be768a03c9dc475579210f3591a4
Mohammed Rezki <mrezki@e2r13p4.1337.ma>
2023-11-07 01:49:57 +0100

    Add: memcpy

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mrezki <mrezki@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/02 05:47:36 by mrezki            #+#    #+#              #
-#    Updated: 2023/11/04 06:39:56 by mrezki           ###   ########.fr        #
+#    Updated: 2023/11/06 19:06:10 by mrezki           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ CFLAGS = -Wall -Wextra -Werror
 RM = rm -f
 SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c \
 	ft_isascii.c ft_isprint.c ft_strlen.c \
-	ft_toupper.c ft_tolower.c \
+	ft_toupper.c ft_tolower.c ft_memcpy.c
 
 OBJS = $(SRCS:.c=.o)
 
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,32 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrezki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/02 05:18:12 by mrezki            #+#    #+#             */
-/*   Updated: 2023/11/06 18:57:31 by mrezki           ###   ########.fr       */
+/*   Created: 2023/11/06 18:25:01 by mrezki            #+#    #+#             */
+/*   Updated: 2023/11/06 19:09:11 by mrezki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <stddef.h>
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+	char	*pdst;
+	char	*psrc;
 
-size_t	ft_strlen(const char *s);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-
-
-#endif
+	i = -1;
+	pdst = (char *)dst;
+	psrc = (char *)src;
+	if (!pdst && !psrc)
+		return (NULL);
+	while (++i < n)
+		*(pdst + i) = *(psrc + i);
+	return (dst);
+}
