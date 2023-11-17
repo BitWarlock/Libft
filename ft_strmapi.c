d58b64ce8c7d07211163153ef79cb6e64b246246
BitWarlock <simorezki@outlook.com>
2023-11-17 02:49:49 +0100

    Add: strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrezki <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 14:58:17 by mrezki            #+#    #+#             */
+/*   Updated: 2023/11/16 17:53:07 by mrezki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
+{
+	char	*ptr;
+	int		i;
+
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	ptr = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!ptr)
+		return (NULL);
+	while (s[i])
+	{
+		ptr[i] = f(i, s[i]);
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
